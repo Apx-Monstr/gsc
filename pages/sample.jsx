65cf0bdeb9dc8fa236c03ab810c9cbeaa90698ad
Apx-Monstr <apxmonstr@gmail.com>
2024-02-16 21:51:48 +0530

    Maps Branch init

--- pages/sample.jsx
+++
@@ -1,9 +0,0 @@
-// accessible at localhost:3000/sample
-
-export default function Sample() {
-    return (
-        <div className="bg-red-500">
-            Hello from Sample Page
-        </div>
-    )
-}
