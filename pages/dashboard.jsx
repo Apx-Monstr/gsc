f645301088b3c9c4f18f173258e8787d4bcb6b7a
Apx-Monstr <apxmonstr@gmail.com>
2024-02-17 17:18:16 +0530

    Maps Setup

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,11 +1,12 @@
 // import dashboardLayout from "@/app/layouts/dashboardLayout";
 import LoggedInLayout from "@/app/layouts/loggedin";
+import Map from "@/components/Map";
 const Dashboard = ()=>{
     return(
         <LoggedInLayout>
             <div className="bg-red-500 w-full h-full flex">
                 <div className="flex-1 bg-green-500">
-
+                    <Map/>
                 </div>
                 <div className="bg-blue-500 w-[30%]">
                     Hello
